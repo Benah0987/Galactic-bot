064430b05d7502a86127bbb1fbff80155e75a5fa
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-30 21:12:23 +0300

    Botspage component creating

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BotsPage from "./BotsPage";
+//import component BOtspage
+
+
+//parent app
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
