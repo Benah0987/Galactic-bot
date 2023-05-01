82e0da784de580ef17d0db2a73f8e255d0de4b27
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-01 22:52:00 +0300

    added a message bar

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -2,19 +2,25 @@ import React from "react";
 import BotCard from "./BotCard";
 
 function YourBotArmy(props) {
+  // Map over the bots in the army and create a BotCard for each one
   const bots = props.bots.map((bot) => {
     return <BotCard key={bot.id} bot={bot} botFunction={props.removeBot} />;
   });
 
   return (
     <div className="ui foarm">
+      {/* Navigation bar */}
       <nav className="navbar bg-dark" data-bs-theme="dark">
         <h1>Army bots</h1>
       </nav>
+
+      {/* Display the bot cards in a grid */}
       <div className="ui five column grid">
         <div className="row bot-army-row">
           {bots}
-          {bots.length === 0 ? <h1></h1> : null}
+
+          {/* If there are no bots in the army, display a message */}
+          {bots.length === 0 ? <h1>You have no bots in your army</h1> : null}
         </div>
       </div>
     </div>
