4c64fc6d677c42b17e06dd5c6b3b721c61aa7e5b
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-02 07:24:17 +0300

    added a catch error func

--- src/components/Botspage.js
+++ src/components/Botspage.js
@@ -3,17 +3,23 @@ import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 const BotsPage = () => {
+  //state that initializes an array to hold all bots
   const [bots, setBots] = useState([]);
+  //state that holds all bots selected by user
   const [yourBots, setYourBots] = useState([]);
-
+ 
+  //useEffect to fetch data
   useEffect(() => {
+    //using async function
     const fetchBots = async () => {
       try {
         const response = await fetch(
           "https://api.npoint.io/f2dab3b71d583e4dbdef/bots"
         );
         const data = await response.json();
+        //store the bots data here
         setBots(data);
+        //if any error due to rendering cconsole log error
       } catch (error) {
         console.log(error.message);
       }
